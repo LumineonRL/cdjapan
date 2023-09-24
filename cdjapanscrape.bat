71dc6532e1a518d80996b21819e8b469f8865bd8
LumineonRL <thebaconator25@aim.com>
2023-09-24 13:19:01 -0400

    Remove hard-coded path in .bat

--- cdjapanscrape.bat
+++ cdjapanscrape.bat
@@ -1,1 +1,21 @@
-"C:\Program Files\R\R-4.2.2\bin\x64\R.exe" CMD BATCH scrape.R
+@echo off
+
+:: Get the R installation path from the registry
+for /F "tokens=2*" %%A in ('reg query "HKLM\SOFTWARE\R-core\R" /v Current Version ^| findstr /i "Current Version"') do (
+    set RVersion=%%B
+)
+
+:: Check if RVersion is set
+if not defined RVersion (
+    echo R is not installed.
+    exit /b
+)
+
+:: Construct the R executable path based on the installation path
+set RExePath=C:\Program Files\R\R-%RVersion%\bin\x64\R.exe
+
+:: Run the R script
+"%RExePath%" CMD BATCH scrape.R
+
+:: Pause to see the output
+pause
