091901343ba150c15077b4f8bac068436ca6f5e1
LumineonRL <thebaconator25@aim.com>
2023-09-24 16:39:38 -0400

    Added error handling for 0x1

--- cdjapanscrape.bat
+++ cdjapanscrape.bat
@@ -1,5 +1,6 @@
 @echo off
 
+:: Check for most recent R installation
 for /F "tokens=2*" %%A in (
     'reg query "HKLM\SOFTWARE\R-core\R" /v InstallPath ^| findstr /i "InstallPath"'
 ) do (
@@ -17,3 +18,13 @@ set RExePath=%RVersion%\bin\x64\R.exe
 
 :: Run the R script
 "%RExePath%" CMD BATCH scrape.R
+
+:: Capture the exit code
+set ExitCode=%errorlevel%
+
+:: Write error to log
+if %ExitCode% equ 1 (
+    echo Task exited with code 1. Performing additional actions...
+    echo Additional actions performed at %date% %time% >> "%~dp0cdjapanerror.log"
+    start "" "%~dp0cdjapanerror.log"
+)
