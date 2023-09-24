43b6ef297f9bd069bef43c1a4ab0c36f741cae42
LumineonRL <thebaconator25@aim.com>
2023-09-24 13:49:20 -0400

    Fixed bat

--- cdjapanscrape.bat
+++ cdjapanscrape.bat
@@ -1,7 +1,8 @@
 @echo off
 
-:: Get the R installation path from the registry
-for /F "tokens=2*" %%A in ('reg query "HKLM\SOFTWARE\R-core\R" /v Current Version ^| findstr /i "Current Version"') do (
+for /F "tokens=2*" %%A in (
+    'reg query "HKLM\SOFTWARE\R-core\R" /v InstallPath ^| findstr /i "InstallPath"'
+) do (
     set RVersion=%%B
 )
 
@@ -12,10 +13,7 @@ if not defined RVersion (
 )
 
 :: Construct the R executable path based on the installation path
-set RExePath=C:\Program Files\R\R-%RVersion%\bin\x64\R.exe
+set RExePath=%RVersion%\bin\x64\R.exe
 
 :: Run the R script
 "%RExePath%" CMD BATCH scrape.R
-
-:: Pause to see the output
-pause
